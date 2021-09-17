e08d490a1ac661338d718d25cf369b78d96b47c6
Jose A. Múrcia <joamuran@gmail.com>
2021-09-17 12:34:50 +0200

    Added PlantillaTema

---
+++ PlantillaTema/templates/webAssets/js/nav.js
@@ -0,0 +1,21 @@
+window.onload = function() {
+    menuOpen = false;
+};
+
+function switchMenu() {
+    if (menuOpen) closeNav();
+    else openNav();
+    menuOpen = !menuOpen;
+
+}
+
+
+/* Set the width of the sidebar to 250px (show it) */
+function openNav() {
+    document.getElementById("mySidepanel").style.width = "250px";
+}
+
+/* Set the width of the sidebar to 0 (hide it) */
+function closeNav() {
+    document.getElementById("mySidepanel").style.width = "0";
+}
